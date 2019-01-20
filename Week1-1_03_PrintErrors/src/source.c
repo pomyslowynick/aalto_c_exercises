ae3c9bb9ad103cd5d9521140aae7ec66759688ea
pomyslowynick <dark0mlet@gmail.com>
2019-01-20 13:48:20 +0000

    Complteted Print Errors

--- Week1-1_03_PrintErrors/src/source.c
+++ Week1-1_03_PrintErrors/src/source.c
@@ -1,9 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "source.h"
-
+#include <string.h>
+#define MAXLEN 100
+#define BREAK_VALUE 101
 void count_sum(void) {
-    int i, j, sum;
-    scanf ("%d %d", &i, &j);
-    sum = i + j;
-    printf("%d + %d = %d\n",i, j, sum);
+    char charsI[MAXLEN], charsJ[MAXLEN];
+    char *end; //what the hell is this, find out
+    int valI, valJ, sum, control;
+    control = 0;
+
+    scanf ("%s %s", charsI, charsJ);
+    valI = strtol(charsI, &end, 10); //strtol made it so easy to solve
+    valJ = strtol(charsJ, &end, 10);
+    if(valI == 0 || valJ == 0) {
+        control = 1;
+        fprintf(stderr, "error\n"); //still not sure about stderr purpose, have to check it out
+    }
+    if (control == 0) {
+        sum = valI + valJ;
+        printf("%s + %s = %d\n", charsI, charsJ, sum);
+
+    }
+
 }
+
+
+// my failed attempt, thought I was so smart but it didn't work for some reason
+    // for(i = 0; i < strlen(charsI); ++i) {
+    //     if ((charsI[i]>='a' && charsI[i]<='z') || (charsI[i]>='A' && charsI[i]<='Z')) {
+    //         fprintf(stderr, "error\n");
+    //         control = 1;
+    //         i = BREAK_VALUE;
+    //         j = BREAK_VALUE;
+    //     }else {
+    //         valI = 10 * valI + (charsI[i] - '0');
+    //     }
+    // }
+
+    // for(; j < strlen(charsJ); ++j) {
+    //     if ((charsJ[j]>='a' && charsJ[j]<='z') || (charsJ[j]>='A' && charsJ[j]<='Z')) {
+    //         fprintf(stderr, "error\n");
+    //         control = 1;
+    //         j = BREAK_VALUE;
+    //     }else {
+    //         valJ = 10 * valJ + (charsJ[j] - '0');
+    //     }
+    // }
--- Week1-1_04_CalcAvg/src/source.c
+++ Week1-1_04_CalcAvg/src/source.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "source.h"
 
+#define MAX_LEN 1000
+
 void calculate_average(){
-    
+    int test, size, sum, i, grade, j;
+    char *ptr;
+    char grades[MAX_LEN];
+    double avg;
+    size = 0;
+    avg = 0.0;
+    test = 1;
+    while (test != -1) {
+        scanf ("%c", grades);
+        
+        for(i = 0; i < MAX_LEN || grades[i] != '\0'; ++i) {
+            j = grades[i] - '0';
+            if (grades[i] == '-1') {
+                test = -1;
+                avg = sum / size;
+            } else {
+                ++size;
+                sum = sum + j;
+
+            }
+        }
+    }
+    printf("%f\n", avg);
 }
