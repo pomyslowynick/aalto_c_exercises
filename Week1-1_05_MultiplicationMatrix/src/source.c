38eb3a8f930e07da33b9d0d1c3d71a4b12ae9e38
pomyslowynick <dark0mlet@gmail.com>
2019-01-20 20:28:52 +0000

    Easy one

--- Week1-1_05_MultiplicationMatrix/src/source.c
+++ Week1-1_05_MultiplicationMatrix/src/source.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 void multiply_matrix(int size){
-    
+    int i, j;
+    i = 1;
+    while(i <= size) {
+        for(j = 1; j <= size; ++j) {
+            printf("%d\t", j * i);
+        }
+        j = 1;
+        ++i;
+        printf("\n");
+    }
 }
