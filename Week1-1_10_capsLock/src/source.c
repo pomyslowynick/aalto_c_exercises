b6cfef84c4a4d7fb38ea586d92df58bb30cfae24
pomyslowynick <dark0mlet@gmail.com>
2019-01-21 17:55:35 +0000

    Starting new exercise

--- Week1-1_10_capsLock/src/source.c
+++ Week1-1_10_capsLock/src/source.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <ctype.h>
 #include "source.h"
-
+#define MAX 100
 void print_capsLock(){
+    char string[MAX];
+
+    scanf("%s", string);
+    printf("%s", string);
 }
