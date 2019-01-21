8d379d96757372129741c1d06d20cc6150f9f1e4
pomyslowynick <dark0mlet@gmail.com>
2019-01-21 13:27:55 +0000

    Changing gitignore

--- Week1-1_02_VariousSums/src/k&r/power.c
+++ Week1-1_02_VariousSums/src/k&r/power.c
@@ -6,6 +6,11 @@ main() {
     printf("%d\n", test);
 }
 
-int power(int m, int n) {
+int power(int base, int n) {
+    int p;
     
+    for (p = 1; n > 0; --n) {
+        p = p * base;
+    }
+    return p;
 }
